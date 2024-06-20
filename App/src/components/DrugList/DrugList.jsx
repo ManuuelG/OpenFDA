d7b3dc358d034749923cc55580e7724c1cb69101
ManuuelG <manuelguerreron93@gmail.com>
2024-06-20 19:02:47 +0200

    Proyecto finalizado

--- App/src/components/DrugList/DrugList.jsx
+++ App/src/components/DrugList/DrugList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Grid from '@mui/material/Grid'
-import { ProductCard } from './ProductCard'
+import ProductCard from '../ProductCard'
 
 const DrugList = ({ medications }) => (
   <Grid container spacing={2}>
